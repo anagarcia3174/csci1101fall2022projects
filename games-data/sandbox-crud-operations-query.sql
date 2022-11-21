fac7938ad55058929411b17d488e2c78fd78cd4a
name <email>
2022-11-21 13:31:08 -0600

    Add games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+SELECT * FROM sandbox;
+
+--INSERT INTO sandbox(int32_value, bool_value, double_value) VALUES (12345, 1, 123456789.10);
+
+--UPDATE sandbox SET string_value = 'hello' WHERE id = 27;
+
+--DELETE FROM sandbox WHERE id = 30;
